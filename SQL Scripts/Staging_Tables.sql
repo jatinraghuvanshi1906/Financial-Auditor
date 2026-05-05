8f57dda77342a310e9d3fcda600196c8fc4c5b25
Jatin <53055322+jatinraghuvanshi1906@users.noreply.github.com>
2026-05-06 00:18:12 +0530

    Add files via upload

---
+++ SQL Scripts/Staging_Tables.sql
@@ -0,0 +1,105 @@
+-- Customers
+CREATE TABLE stg_customers (
+    customer_id TEXT,
+    customer_fname TEXT,
+    customer_lname TEXT,
+    email TEXT,
+    phone TEXT,
+    created_at TEXT,
+    status TEXT
+);
+
+-- Transactions
+CREATE TABLE stg_transactions (
+    transaction_id TEXT,
+    customer_id TEXT,
+    account_id TEXT,
+    amount TEXT,
+    transaction_type TEXT,
+    status TEXT,
+    payment_method_id TEXT,
+    reference_id TEXT,
+    created_at TEXT
+);
+
+-- Invoices
+CREATE TABLE stg_invoices (
+    invoice_id TEXT,
+    customer_id TEXT,
+    total_amount TEXT,
+    invoice_date TEXT,
+    status TEXT,
+    created_at TEXT
+);
+
+-- Payments
+CREATE TABLE stg_payments (
+    payment_id TEXT,
+    invoice_id TEXT,
+    amount TEXT,
+    payment_date TEXT,
+    payment_method_id TEXT,
+    status TEXT,
+    reference_id TEXT
+);
+
+-- Payment Methods
+CREATE TABLE stg_payment_methods (
+    method_id TEXT,
+    method_type TEXT,
+    provider TEXT
+);
+
+-- Currency
+CREATE TABLE stg_currency (
+    currency_code TEXT,
+    currency_name TEXT,
+    exchange_rate TEXT
+);
+
+-- Fraud Flags
+CREATE TABLE stg_fraud_flags (
+    flag_id TEXT,
+    transaction_id TEXT,
+    issue_type TEXT,
+    severity TEXT,
+    created_at TEXT
+);
+
+-- Audit Logs
+CREATE TABLE stg_audit_logs (
+    log_id TEXT,
+    table_name TEXT,
+    record_id TEXT,
+    audit_action TEXT,
+    old_value TEXT,
+    new_value TEXT,
+    changed_at TEXT
+);
+
+-- Accounts
+CREATE TABLE stg_accounts (
+    account_id TEXT,
+    customer_id TEXT,
+    account_type TEXT,
+    balance TEXT,
+    status TEXT,
+    created_at TEXT
+);
+
+-- Transaction Items
+CREATE TABLE stg_transaction_items (
+    item_id TEXT,
+    transaction_id TEXT,
+    product_id TEXT,
+    quantity TEXT,
+    price TEXT
+);
+
+-- Products
+CREATE TABLE stg_products (
+    product_id TEXT,
+    product_name TEXT,
+    category TEXT,
+    price TEXT
+);
